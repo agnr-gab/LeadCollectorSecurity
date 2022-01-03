28d47f042014893ff14e7257512102993c864acd
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-03 14:07:24 -0300

    Criar Service do Usuario

--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioService.java
@@ -1,2 +1,10 @@
-package br.com.zup.LeadCollector.usuario;public class UsuarioService {
+package br.com.zup.LeadCollector.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
 }
