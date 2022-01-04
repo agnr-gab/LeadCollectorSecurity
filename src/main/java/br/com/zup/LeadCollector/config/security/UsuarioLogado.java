2dde84e34ec5fe65663fdfa675e6959132bc9497
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-04 15:49:04 -0300

    Criar classe para configurar a auntentificação do usuario

--- src/main/java/br/com/zup/LeadCollector/config/security/UsuarioLogado.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/UsuarioLogado.java
@@ -1,2 +1,83 @@
-package br.com.zup.LeadCollector.config.security;public class UsuarioLogado {
+package br.com.zup.LeadCollector.config.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class UsuarioLogado implements UserDetails {
+    private UUID id;
+    private String email;
+    private String senha;
+
+    public UsuarioLogado(UUID id, String email, String senha) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public UsuarioLogado() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    // metodos obrigatórios
+
+    @Override //quais as autorizações o usuário tem que ter
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override //comparar a senha no banco de dados
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override //comparar o usuário no banco de dados
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
