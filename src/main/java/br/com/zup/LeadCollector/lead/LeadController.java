2e1604acfb7368b4ec46ca28d3c74e2e43a1fd8c
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-04 16:04:39 -0300

    Criar controller da lead

--- src/main/java/br/com/zup/LeadCollector/lead/LeadController.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadController.java
@@ -1,2 +1,24 @@
-package br.com.zup.LeadCollector.lead;public class LeadController {
+package br.com.zup.LeadCollector.lead;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/leads")
+public class LeadController {
+    @Autowired
+    private LeadService leadService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    //por questão de serguraça nunca retorna a model fazer um DTO posteriormente
+    public Lead cadastrarLead (@RequestBody Lead lead){
+        return leadService.salvarLEad(lead);
+    }
+
+    @GetMapping
+    public Iterable<Lead> exibirTodosLeads() {
+        return leadService.exibirLeads();
+    }
 }
