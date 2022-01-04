b805b1a613e718df65819b8ddbbd130001871d17
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-04 15:53:54 -0300

    Criar DTO de login

--- src/main/java/br/com/zup/LeadCollector/usuario/dtos/LoginDTO.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/dtos/LoginDTO.java
@@ -1,2 +1,25 @@
-package br.com.zup.LeadCollector.usuario.dtos;public class LoginDTO {
+package br.com.zup.LeadCollector.usuario.dtos;
+
+public class LoginDTO {
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LoginDTO() {
+    }
 }
