c230ae28c8122380d9974c77a83cb58b3372e39f
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-04 16:06:11 -0300

    Criar a controller de usuario

--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
@@ -1,13 +1,25 @@
 package br.com.zup.LeadCollector.usuario;
 
+import br.com.zup.LeadCollector.usuario.dtos.CadastroUsuarioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario") //como terá apenas acesso ao dado de um usuario usar no singular
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarUsuario(@RequestBody CadastroUsuarioDTO cadastroUsuarioDTO) {
+        Usuario usuario = new Usuario(); //utilizar model mapper aqui
+        usuario.setEmail(cadastroUsuarioDTO.getEmail()); //utilizar model mapper aqui
+        usuario.setSenha(cadastroUsuarioDTO.getSenha()); //utilizar model mapper aqui
+        /*falha de segurança: retornar a model retorna a senha do usuario
+        return usuarioService.salvarUsuario(usuario);*/
+        usuarioService.salvarUsuario(usuario);
+    }
 
 }
