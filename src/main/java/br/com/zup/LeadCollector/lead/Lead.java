029bc72dc4e7816263a51d98871be056025aaf04
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-04 15:54:38 -0300

    Criar model lead

--- src/main/java/br/com/zup/LeadCollector/lead/Lead.java
+++ src/main/java/br/com/zup/LeadCollector/lead/Lead.java
@@ -1,2 +1,42 @@
-package br.com.zup.LeadCollector.lead;public class Lead {
+package br.com.zup.LeadCollector.lead;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Lead {
+
+    @Id
+    @Column(nullable = false)
+    private String nome;
+    private String email;
+    private String observacoes;
+
+    public Lead() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
 }
