89f717b0cd5e5275974f87b0174c1178233087c4
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-04 16:05:35 -0300

    Criar service da lead

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -1,2 +1,18 @@
-package br.com.zup.LeadCollector.lead;public class LeadService {
+package br.com.zup.LeadCollector.lead;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeadService {
+    @Autowired
+    private LeadRepository leadRepository;
+
+    public Lead salvarLEad(Lead lead) {
+        return leadRepository.save(lead);
+    }
+
+    public Iterable<Lead> exibirLeads() {
+        return leadRepository.findAll();
+    }
 }
