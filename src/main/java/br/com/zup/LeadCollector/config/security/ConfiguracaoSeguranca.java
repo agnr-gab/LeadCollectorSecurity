d7621399be5043fe6c118f608b9069c11d1bfd93
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-04 11:03:54 -0300

    Inserir configuração de segurança

--- src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoSeguranca.java
@@ -1,2 +1,21 @@
-package br.com.zup.LeadCollector.config.security;public class ConfiguracaoSeguranca {
+package br.com.zup.LeadCollector.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http); não vamos utilizar o metodo padrão personalizavel
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/leads")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
 }
