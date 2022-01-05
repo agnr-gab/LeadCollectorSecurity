231cca571f7bce332d97d6d7f48b06b52b9f272a
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-05 13:40:45 -0300

    Criar DTO da lead

---
+++ src/main/java/br/com/zup/LeadCollector/lead/dtos/LeadDTO.java
@@ -0,0 +1,34 @@
+package br.com.zup.LeadCollector.lead.dtos;
+
+public class LeadDTO {
+    private String nome;
+    private String email;
+    private String observacoes;
+
+    public LeadDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+}
