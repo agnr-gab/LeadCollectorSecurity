fc820a63892909463f5b9b39dc85cd9144cbbeed
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-05 16:38:05 -0300

    inserir configuração cors

--- src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoSeguranca.java
@@ -1,12 +1,17 @@
 package br.com.zup.LeadCollector.config.security;
 
+import br.com.zup.LeadCollector.config.security.JWT.FiltroDeAutenticacaoJWT;
+import br.com.zup.LeadCollector.config.security.JWT.JWTComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -15,6 +20,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JWTComponent jwtComponent;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     private static final String[] ENDPOINT_POST_PUBLICO = {
             "/usuario",
             "/leads"
@@ -33,6 +44,13 @@ public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
                 .authenticated();
         http.sessionManagement().
                 sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilter(new FiltroDeAutenticacaoJWT(jwtComponent, authenticationManager()));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
