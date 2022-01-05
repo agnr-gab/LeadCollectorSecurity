7c43e569d9ba4583c7b7e1a48b148597920d577c
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-05 15:12:39 -0300

    Classe filtro de autentificação

---
+++ src/main/java/br/com/zup/LeadCollector/config/security/JWT/FiltroDeAutenticacaoJWT.java
@@ -0,0 +1,44 @@
+package br.com.zup.LeadCollector.config.security.JWT;
+
+import br.com.zup.LeadCollector.config.security.JWT.exceptions.AcessoNegadoException;
+import br.com.zup.LeadCollector.usuario.dtos.LoginDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilter {
+    private JWTComponent jwtComponent;
+    private AuthenticationManager authenticationManager;
+
+    public FiltroDeAutenticacaoJWT(JWTComponent jwtComponent, AuthenticationManager authenticationManager) {
+        this.jwtComponent = jwtComponent;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            LoginDTO loginDTO = objectMapper.readValue(request.getInputStream(), LoginDTO.class);
+
+            //verifica se os dados inseridos confere com os dados que estão no banco de dados para conceder acesso ou não
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDTO
+                    .getEmail(), loginDTO.getSenha(), new ArrayList<>());
+
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            return authentication;
+
+        } catch (IOException ioException) {
+            throw new AcessoNegadoException();
+        }
+    }
+}
---
+++ src/main/java/br/com/zup/LeadCollector/config/security/JWT/exceptions/AcessoNegadoException.java
@@ -0,0 +1,2 @@
+package br.com.zup.LeadCollector.config.security.JWT.exceptions;public class AcessoNegadoException {
+}
