63d9a00343858a4ad1035890253bae8bfc6a0ef6
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-06 11:19:06 -0300

    Criar metodo de pegar autorização do token

--- src/main/java/br/com/zup/LeadCollector/config/security/JWT/FiltroDeAutorizaçãoJWT.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/JWT/FiltroDeAutorizaçãoJWT.java
@@ -1,18 +1,36 @@
 package br.com.zup.LeadCollector.config.security.JWT;
 
+import br.com.zup.LeadCollector.config.security.JWT.exceptions.TokenInvalidoException;
+import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 public class FiltroDeAutorizaçãoJWT extends BasicAuthenticationFilter {
     private JWTComponent jwtComponent;
-    private UserDetailsService userDetailsService; // Liskov Substitution Principle (LSP)  diz que objetos podem ser substituídos por seus subtipos sem que isso afete a execução correta do programa
+    private UserDetailsService userDetailsService; // Liskov Substitution Principle (LSP)  diz que objetos podem ser
+    // substituídos por seus subtipos sem que isso afete a execução correta do programa
 
 
-    public FiltroDeAutorizaçãoJWT(AuthenticationManager authenticationManager, JWTComponent jwtComponent, UserDetailsService userDetailsService) {
+    public FiltroDeAutorizaçãoJWT(AuthenticationManager authenticationManager, JWTComponent jwtComponent,
+                                  UserDetailsService userDetailsService) {
         super(authenticationManager);
         this.jwtComponent = jwtComponent;
         this.userDetailsService = userDetailsService;
+    }
+
+    public UsernamePasswordAuthenticationToken pegarAutentificacao(String token) {
+        if (!jwtComponent.tokenValido(token)) {
+            throw new TokenInvalidoException();
+        }
+
+        Claims claims = jwtComponent.pegarClaims(token);
+        UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
+
     }
 }
